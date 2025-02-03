9722195cfd6e6dd6a5649bef3a2dc10b6bf11f29
okan <okan.tasin@hotmail.com>
2025-02-03 23:23:38 +0300

    @EnableMethodSecurity and test preAuthorize , postAuthorize annatotion

--- src/main/java/com/template/resourceserver/controller/UserController.java
+++ src/main/java/com/template/resourceserver/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.template.resourceserver.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.template.resourceserver.dto.UserResponse;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -13,5 +17,21 @@ public class UserController {
     @GetMapping("/status/check")
     public String statusCheck() {
         return "OK";
+    }
+
+    // @Secured("ROLE_user")
+    @PreAuthorize("hasAuthority('ROLE_developer') or #userId == #jwt.subject")
+    @DeleteMapping(path = "/{userId}")
+    public String deleteUser(@PathVariable String userId, @AuthenticationPrincipal Jwt jwt) {
+        return "Deleted user: " + userId + " from " + jwt.getSubject();
+
+    }
+
+    // @Secured("ROLE_user")
+    @PostAuthorize("returnObject.userId == #jwt.subject")
+    @GetMapping(path = "/{userId}")
+    public UserResponse getUser(@PathVariable String userId, @AuthenticationPrincipal Jwt jwt) {
+        return new UserResponse("Okan","tasin","994dec25-a13f-490f-97c7-2de8389ddde3");
+
     }
 }
