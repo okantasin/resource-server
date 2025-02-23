e0ff4d003fab698402a680a4b2287fdea98eeaad
okantasin <okan.tasin@hotmail.com>
2025-02-23 03:05:38 +0300

    changed api gateway conf

--- src/main/java/com/template/resourceserver/controller/UserController.java
+++ src/main/java/com/template/resourceserver/controller/UserController.java
@@ -2,21 +2,28 @@ package com.template.resourceserver.controller;
 
 
 import com.template.resourceserver.dto.UserResponse;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    Environment environment;
 
     @GetMapping("/status/check")
-    public String statusCheck() {
-        return "OK";
+    public ResponseEntity<String> statusCheck() {
+        return  ResponseEntity.ok("Working on port " + environment.getProperty("local.server.port"));
     }
 
     // @Secured("ROLE_user")
